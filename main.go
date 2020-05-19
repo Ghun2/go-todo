45c4a233c125515a4cdef5c161a5086029f25145
ghun <ghun2ee@gmail.com>
2020-05-19 18:21:48 +0900

    use port env var

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"go-todo/app"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
 	m := app.MakeHandler("./todo.db")
 	defer m.Close()
 
-	log.Fatal(http.ListenAndServe(":3000", m))
+	log.Fatal(http.ListenAndServe(":" + port, m))
 }
